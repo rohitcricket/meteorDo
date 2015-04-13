485eea12e1a0c6d23b48ac6dd9f84f9c7fe4c974
Rohit Gupta <rohitcricket@gmail.com>
2015-04-12 21:16:43 -0700

    added user accounts

--- simple-todos.js
+++ simple-todos.js
@@ -1,31 +1,35 @@
-// simple-todos.js
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
-  Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks first
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
-
-  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
   Template.body.events({
     "submit .new-task": function (event) {
       // This function is called when the new task form is submitted
-
       var text = event.target.text.value;
 
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
@@ -33,10 +37,12 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
-    // In the client code, below everything else
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
@@ -47,6 +53,8 @@ if (Meteor.isClient) {
     }
   });
 
-
-} // end of if statement.
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
 
